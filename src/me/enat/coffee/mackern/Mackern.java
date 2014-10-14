346436ad956e6e7deb640961c2c2373c3d82af66
Yannic Remmet <git@meee.de>
2014-10-14 18:28:35 +0200

    Fixed Bug

--- src/me/enat/coffee/mackern/Mackern.java
+++ src/me/enat/coffee/mackern/Mackern.java
@@ -1,7 +1,7 @@
 package me.enat.coffee.mackern;
 
 import java.io.File;
-import java.lang.reflect.InvocationHandler;
+import java.lang.*;import java.lang.Class;import java.lang.ClassLoader;import java.lang.ClassNotFoundException;import java.lang.Exception;import java.lang.IllegalAccessException;import java.lang.IllegalArgumentException;import java.lang.NoSuchMethodException;import java.lang.Object;import java.lang.Override;import java.lang.SecurityException;import java.lang.String;import java.lang.System;import java.lang.Throwable;import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -34,8 +34,8 @@ public class Mackern {
 
                 @Override
                 public java.lang.Object invoke(Object proxy, Method method, Object[] args)
-                        throws Throwable {
-                    Class<?> class_openFilesEvent = getEventClassWithName(EVENT_TYPES.OPEN_URI_EVENT.handlerClassName);
+                        throws java.lang.Throwable {
+                    Class<?> class_openFilesEvent = getEventClassWithName(EVENT_TYPES.OPEN_URI_EVENT.eventClassname);
                     Method filesMethod = class_openFilesEvent.getMethod("getURI");
                     URI uri = (URI) filesMethod.invoke(args[0]);
                     openURIEventHandler.recivedOpenURIEvent(uri);
@@ -58,7 +58,7 @@ public class Mackern {
                 @Override
                 public Object invoke(Object proxy, Method method, Object[] args)
                         throws Throwable {
-                    Class<?> class_openFilesEvent = getEventClassWithName(EVENT_TYPES.OPEN_FILES_EVENT.handlerClassName);
+                    Class<?> class_openFilesEvent = getEventClassWithName(EVENT_TYPES.OPEN_FILES_EVENT.eventClassname);
                     Method filesMethod = class_openFilesEvent.getMethod("getFiles");
                     List<File> files = (List<File>) filesMethod.invoke(args[0]);
                     openFileEventHandler.recivedOpenFileEvent(files);
